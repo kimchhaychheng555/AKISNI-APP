d70eedffa8438edf8d08452cbf870c93cf9d7138
sokkakada32 <72429607+kimchhaychheng555@users.noreply.github.com>
2022-10-23 14:49:01 +0700

    changes

--- akisni_app/lib/views/manage_views/manage_view.dart
+++ akisni_app/lib/views/manage_views/manage_view.dart
@@ -1,10 +1,7 @@
 import 'package:akisni_app/components/date_time_picker_component.dart';
 import 'package:akisni_app/controllers/manage_controller.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:get/get.dart';
-
 import '../../components/card_component.dart';
 import '../../components/input_text_component.dart';
 import '../../components/loading_overlay_component.dart';
@@ -32,76 +29,74 @@ class ManageView extends StatelessWidget {
         ),
       ),
       body: LoadingOverlayComponent(
-        child: Padding(
+        child: SingleChildScrollView(
           padding: EdgeInsets.all(DEFAULT_PADDING),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                CardComponent(
-                  title: 'general_information'.tr,
-                  isHasTitle: true,
-                  child: Column(
-                    children: [
-                      DateTimePickerComponent(
-                        onSelectonChange: controller.onSelectChangeDate,
-                      ),
-                      InputTextComponent(
-                        placeholder: 'customer_name',
-                      ),
-                      InputTextComponent(
-                        placeholder: 'company_name'.tr,
-                      ),
-                      InputTextComponent(
-                        isTextArea: true,
-                        placeholder: 'location'.tr,
-                      ),
-                    ],
-                  ),
+          child: Column(
+            children: [
+              CardComponent(
+                title: 'general_information'.tr,
+                isHasTitle: true,
+                child: Column(
+                  children: [
+                    DateTimePickerComponent(
+                      onSelectonChange: controller.onSelectChangeDate,
+                    ),
+                    InputTextComponent(
+                      placeholder: 'customer_name',
+                    ),
+                    InputTextComponent(
+                      placeholder: 'company_name'.tr,
+                    ),
+                    InputTextComponent(
+                      isTextArea: true,
+                      placeholder: 'location'.tr,
+                    ),
+                  ],
                 ),
-                CardComponent(
-                  title: 'marches_information'.tr,
-                  isHasTitle: true,
-                  child: Column(
-                    children: [
-                      InputTextComponent(
-                        placeholder: 'company_name'.tr,
-                      ),
-                      InputTextComponent(
-                        placeholder: 'location_name'.tr,
-                      ),
-                    ],
-                  ),
+              ),
+              CardComponent(
+                title: 'marches_information'.tr,
+                isHasTitle: true,
+                child: Column(
+                  children: [
+                    InputTextComponent(
+                      placeholder: 'company_name'.tr,
+                    ),
+                    InputTextComponent(
+                      placeholder: 'location_name'.tr,
+                    ),
+                  ],
                 ),
-                CardComponent(
-                  title: 'map_information'.tr,
-                  isHasTitle: true,
-                  child: Column(
-                    children: [
-                      InputTextComponent(
-                        placeholder: 'company_name'.tr,
-                      ),
-                      InputTextComponent(
-                        placeholder: 'location_name'.tr,
-                      ),
-                    ],
-                  ),
+              ),
+              CardComponent(
+                title: 'map_information'.tr,
+                isHasTitle: true,
+                child: Column(
+                  children: [
+                    InputTextComponent(
+                      placeholder: 'company_name'.tr,
+                    ),
+                    InputTextComponent(
+                      placeholder: 'location_name'.tr,
+                    ),
+                  ],
                 ),
-                CardComponent(
-                  title: 'image'.tr,
-                  isHasTitle: true,
-                  child: Column(
-                    children: [
-                      InputTextComponent(
-                        placeholder: 'company_name'.tr,
-                      ),
-                      InputTextComponent(
-                        placeholder: 'location_name'.tr,
-                      ),
-                    ],
-                  ),
+              ),
+              CardComponent(
+                title: 'image'.tr,
+                isHasTitle: true,
+                child: Column(
+                  children: [
+                    InputTextComponent(
+                      placeholder: 'company_name'.tr,
+                    ),
+                    InputTextComponent(
+                      placeholder: 'location_name'.tr,
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
