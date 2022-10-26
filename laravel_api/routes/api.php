50bab34a83d134790fef44c2382f2d2b251dc8e6
kimchhaychheng555 <72429607+kimchhaychheng555@users.noreply.github.com>
2022-10-26 21:08:43 +0700

    changes

--- laravel_api/routes/api.php
+++ laravel_api/routes/api.php
@@ -2,9 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\ImageController;
-
 
 /*
 |--------------------------------------------------------------------------
@@ -17,29 +14,6 @@ use App\Http\Controllers\ImageController;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
-# IMAGE
-Route::post("image", function (Request $request) {
-    return ImageController::upload($request);
-});
-
-
-# USER API
-Route::get("user", function () {
-    return UserController::getUser();
-});
-Route::get('user/{id}', function ($id) {
-    return UserController::findOne($id);
-});
-Route::post('user', function (Request $request) {
-    return UserController::createUser($request);
-});
-Route::post('user/update', function (Request $request) {
-    return UserController::updateUser($request);
-});
-Route::post('user/login', function (Request $request) {
-    return UserController::login($request);
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
 });
