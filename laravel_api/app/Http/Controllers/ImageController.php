0122532138401b11d7ba136cc1f462bcf1619c53
kimchhaychheng555 <72429607+kimchhaychheng555@users.noreply.github.com>
2022-10-26 19:17:20 +0700

    changes

--- laravel_api/app/Http/Controllers/ImageController.php
+++ laravel_api/app/Http/Controllers/ImageController.php
@@ -3,20 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Str; 
+
 
 class ImageController extends Controller
 {
-    function upload(Request $request){
+    public function upload(Request $request)
+    {         
+
+        $imageName = Str::uuid()->toString().'.'.$request->file('file')->extension();  
+        $result = $request->file('file')->storeAs('uploads', $imageName);
+
+
+        return ["result"=>$result];
+        // $request->validate([
+        //     'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        // ]);
+
+    
+        // // $result = $request->file('file')->move(public_path('images'), $imageName);
+
         
-        $validatedData = $request->validate([
-            'image' => 'required|image|mimes:jpg,png,jpeg,gif,svg|max:2048',
-    
-           ]);
-    
-        $name = $request->file('image')->getClientOriginalName();
+        // // return ["result"=>$result];
 
-        $path = $request->file('image')->store('public/images');
+        // $imageName = Str::uuid()->toString().'.'.$request->image->extension();  
+        // $result = $request->file('file')->store('uploads');
+        // return ["result"=>$result];
+    }
 
-        return  ["result"=> ('upload-image')->with('status', 'Image Has been uploaded')];
-    } 
 }
