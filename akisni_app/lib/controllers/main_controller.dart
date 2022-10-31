1f2611ab2c2a98a716f0c7c9a5639ea8ad8f93c1
kimchhaychheng555 <72429607+kimchhaychheng555@users.noreply.github.com>
2022-10-31 20:36:59 +0700

    changes

--- akisni_app/lib/controllers/main_controller.dart
+++ akisni_app/lib/controllers/main_controller.dart
@@ -56,6 +56,15 @@ class MainController extends FullLifeCycleController {
       return Future.error(
           'Location permissions are permanently denied, we cannot request permissions.');
     }
+
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(function(position){
+            var positionInfo = "${"Your current position is (" + "Latitude: " + position.coords.latitude + ", " + "Longitude: " + position.coords.longitude})";
+            document.getElementById("result").innerHTML = positionInfo;
+        });
+    } else{
+        alert("Sorry, your browser does not support HTML5 geolocation.");
+    }
   }
 
   Widget get afterLoading {
